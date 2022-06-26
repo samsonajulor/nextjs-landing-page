30fd709e52a2e685f940a17215d16978aa2c3fdb
ejioguvictor <ejioguvictor6054@gmail.com>
2022-06-26 19:59:12 +0100

    updated file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,38 +3,14 @@ import Router from 'next/router';
 import { initGA, logPageView } from 'analytics';
 import 'swiper/swiper-bundle.min.css';
 import 'rc-drawer/assets/index.css';
-import 'typeface-dm-sans';import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import 'typeface-dm-sans';
 
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
     initGA();
     logPageView();
     Router.events.on('routeChangeComplete', logPageView);
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
   }, []);
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
-      </Head>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-    </React.Fragment>
-  );
+  return <Component {...pageProps} />;
 }
-
-
-CustomApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
